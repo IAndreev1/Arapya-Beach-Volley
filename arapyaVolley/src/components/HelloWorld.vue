c2c194f992de51899770ca8d7fb77a1583fa1d4b
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-06-23 19:57:02 +0200

    Start

--- arapyaVolley/src/components/HelloWorld.vue
+++ arapyaVolley/src/components/HelloWorld.vue
@@ -15,6 +15,7 @@ defineProps({
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <img src="./icons/sofia-1.jpg">
   </div>
 </template>
 
