f5038e26180f74e0aa5ad45130d10b7bac3e1209
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-06-23 20:01:55 +0200

    Start

--- arapyaVolley/src/components/HelloWorld.vue
+++ arapyaVolley/src/components/HelloWorld.vue
@@ -8,15 +8,9 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-    <img src="./icons/sofia-1.jpg">
-  </div>
+
+
+
 </template>
 
 <style scoped>
