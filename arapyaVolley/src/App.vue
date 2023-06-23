356ae9e459fef7d7666d319b1d5e9bb0ca596a1a
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-06-23 19:48:33 +0200

    Start

--- arapyaVolley/src/App.vue
+++ arapyaVolley/src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { inject } from '@vercel/analytics';
+
+export default () => {
+  inject();
+};
+
 </script>
 
 <template>
