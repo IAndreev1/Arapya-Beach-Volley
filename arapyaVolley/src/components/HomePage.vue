2d757f325cc7cacc5f8c17135c8f7a844e420234
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-06-25 11:13:40 +0200

    Basic start ideas.

--- arapyaVolley/src/components/HomePage.vue
+++ arapyaVolley/src/components/HomePage.vue
@@ -1,6 +1,7 @@
 <template>
   <head>
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body id = "MyBody">
 
