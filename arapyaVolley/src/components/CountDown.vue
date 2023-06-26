eb1e4951a90ebc24dd3ebc6cbd676c101ba58800
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-06-26 11:22:07 +0200

    Max Hight Photos 100% grid cell

--- arapyaVolley/src/components/CountDown.vue
+++ arapyaVolley/src/components/CountDown.vue
@@ -36,37 +36,40 @@ export default {
   },
   methods: {
     startCountdown() {
-      const targetDate = new Date('2023-07-29T10:00:00');// Replace with your target date and time
-      const now = new Date();
-      console.log(now)
+      const targetDate = new Date('2023-07-29T10:00:00'); // Replace with your target date and time
 
-      const difference = targetDate - now;
+      setInterval(() => {
+        const now = new Date();
+        const difference = targetDate - now;
 
-      if (difference > 0) {
-        Countdown(new Date(targetDate), (ts) => {
-          console.log(ts.days)
+        if (difference > 0) {
+          const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+          const minutes = Math.floor((difference / 1000 / 60) % 60);
+          const seconds = Math.floor((difference / 1000) % 60);
+
           this.countdownValue = {
-            days: ts.days,
-            hours: ts.hours,
-            minutes: ts.minutes,
-            seconds: ts.seconds,
+            days,
+            hours,
+            minutes,
+            seconds,
           };
-
-        });
-      } else {
-        // Countdown has already reached the target date
-        this.countdownValue = {
-          days: 0,
-          hours: 0,
-          minutes: 0,
-          seconds: 0,
-        };
-      }
+        } else {
+          // Countdown has already reached the target date
+          this.countdownValue = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+          };
+        }
+      }, 1000);
     },
   },
   mounted() {
     this.startCountdown();
   },
+
 };
 </script>
 
