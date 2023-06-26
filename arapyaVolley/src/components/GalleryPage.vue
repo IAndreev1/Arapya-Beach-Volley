5e7964fa67178c7ccde659f4292e08f137adbf8b
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-06-26 09:57:36 +0200

    Start Page  + GalleryPage.vue

--- arapyaVolley/src/components/GalleryPage.vue
+++ arapyaVolley/src/components/GalleryPage.vue
@@ -1,9 +1,19 @@
 <template>
-  <NavBar activeStart="yes" active-gallery="no" active-next="no" class="header-class"/>
-  <div className="gallery">
-    <img v-for="(image, index) in images" :key="index" :src="image.src" :alt="image.alt">
+  <NavBar activeStart="no" active-gallery="yes" active-next="no" class="header-class" />
+  <div class="gallery">
+    <img src="./icons/ekip.jpg" @click="toggleImageSize">
+    <img src="./icons/dronSnimka.jpg" @click="toggleImageSize">
+    <img src="./icons/shamp.jpg" @click="toggleImageSize">
+    <img src="./icons/nagradi.jpg" @click="toggleImageSize">
+    <img src="./icons/Pobediteli.jpg" @click="toggleImageSize">
+    <img src="./icons/logo123.jpg" @click="toggleImageSize">
+    <img src="./icons/nagradi.jpg" @click="toggleImageSize">
+    <!-- Add more image sources here -->
   </div>
-  <Footer/>
+  <div v-if="isImageEnlarged" class="enlarged-image-container" @click="toggleImageSize">
+    <img :src="enlargedImageSrc" class="enlarged-image" />
+  </div>
+  <Footer />
 </template>
 
 <script>
@@ -11,16 +21,18 @@ import NavBar from "@/components/NavBar.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
-  components: {Footer, NavBar},
+  components: { Footer, NavBar },
   data() {
     return {
-      images: [
-        {src: "src/components/icons/ekip.jpg", alt: "Image 1"},
-        {src: "path/to/image2.jpg", alt: "Image 2"},
-        {src: "path/to/image3.jpg", alt: "Image 3"}
-        // Add more images here
-      ]
+      isImageEnlarged: false,
+      enlargedImageSrc: ""
     };
+  },
+  methods: {
+    toggleImageSize(event) {
+      this.isImageEnlarged = !this.isImageEnlarged;
+      this.enlargedImageSrc = event.target.src;
+    }
   }
 };
 </script>
@@ -30,10 +42,34 @@ export default {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 20px;
+  align-items: center;
 }
 
 .gallery img {
   width: 100%;
-  height: auto;
+  padding: 1em;
+  max-height: 30em;
+  object-fit: cover;
+  border-radius: 10%;
+  cursor: pointer;
+}
+
+.enlarged-image-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 9999;
+  cursor: pointer;
+}
+
+.enlarged-image {
+  max-height: 80vh;
+  max-width: 80vw;
 }
 </style>
