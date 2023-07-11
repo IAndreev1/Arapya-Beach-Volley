6c0fc955e512c7b4bdcef0111b9b3a074fb9993d
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-07-11 14:12:33 +0300

    FInal

--- arapyaVolley/src/components/ContactForm.vue
+++ arapyaVolley/src/components/ContactForm.vue
@@ -1,27 +1,143 @@
 <template>
-  <NavBar activeStart="no" active-gallery="no" active-next="yes" class="header-class"/>
-  <div class="contact-page">
-    <h1>Contact Information</h1>
-    <div class="contact-details">
-      <div class="info-item">
-        <span class="label">Organization Name:</span>
-        <span class="value">My Organization</span>
-      </div>
-      <div class="info-item">
-        <span class="label">Address:</span>
-        <span class="value">123 Main Street, City, Country</span>
-      </div>
-      <div class="info-item">
-        <span class="label">Phone:</span>
-        <span class="value">+1 234 567 890</span>
-      </div>
-      <div class="info-item">
-        <span class="label">Email:</span>
-        <span class="value">info@myorganization.com</span>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <div class="block-content">
+    <div class="text text-center">
+      <div class="grid-container">
+        <!-- Full-width текст и медиа -->
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="./1_1 (1).jpg"></figure>
+          <h1>2800W Номинална мощност</h1>
+          <p>Два елeктро мотора с мощност 1400W, които предоставят максимална скорост от 75км/ч.</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="./1_2 (1).jpg"></figure>
+          <h1>32A Регулатор на скоростта</h1>
+          <p>Осигурява сила и мощ за изкачване до 30ᵒ градусoви наклони</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="./1_3 (1).jpg"></figure>
+          <h1>Единичен/двоен двигател</h1>
+          <p>Спестете енергия от батерията, като преминете към режим на еко задвижване с един мотор или се
+            насладете
+            на максимален потенциал, като използвате високоефективния режим на каране с два двигателя</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="./2_1 (1).jpg"></figure>
+          <h1>16 x 8 литиево-йоннa батерия</h1>
+          <p>MUKUTA със 128 литиево-йонни клетки с висока енергийна плътност 18650. В състояние да достигне до
+            100км/120км пробег на дълги разстояния</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="./2_2 (1).jpg"></figure>
+          <h1>2 Входа за зареждане</h1>
+          <p>Намалете времето за зареждане наполовина, като използвате две зарядни устройства едновременно
+          </p>
+        </div>
+        <hr/>
+
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="3_1 (1).jpg"></figure>
+          <h1>Окачване с 4 пружини</h1>
+          <p>Абсорбцията на 4 пружини прави вашето каране по-безопасно и по-удобно</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="3_2 (1).jpg"></figure>
+          <h1>Независимо окачване</h1>
+          <p>Предното и задното независимо окачване могат да поемат значителни неравности и да поддържат
+            тротинетката стабилна</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="3_3.jpg"></figure>
+          <h1>Максимално натоварване</h1>
+          <p>Висококачествената система на окачването позволява на тротинетката максимално натоварване от 150
+            килограма.</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="4_1 (1).jpg"></figure>
+          <h1>Двойна хидравлична спирачка</h1>
+          <p>Хидравличните дискови спирачки осигуряват най-голяма спирачна сила, което ги прави най-добрите за
+            вашата безопасност</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="4_2 (1).jpg"></figure>
+          <h1>145 MM спирачен диск</h1>
+          <p>145 мм дискова спирачка осигурява ултра къс спирачен път с много малко усилие, по-лесна е за смяна и
+            е по-ефективна от барабанните спирачки</p>
+        </div>
+        <hr/>
+
+
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="5_4 (1).jpg"></figure>
+          <h1>Борд против хлъзване</h1>
+          <p>Гумена постелка на борда, против хлъзгане, лесна за почистване, дълъг експлоатационен живот</p>
+        </div>
+
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="5_1 (1).jpg"></figure>
+          <h1>2 LED предни светлини</h1>
+          <p></p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="5_2 (1).jpg"></figure>
+          <h1>Задна светлина</h1>
+          <p></p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="5_3.jpg"></figure>
+          <h1>Предни и задни мигачи</h1>
+          <p></p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="5_4 (1).jpg"></figure>
+          <h1>Странични светлини</h1>
+          <p></p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="3.jpg"></figure>
+          <h1>Интелигентен контрол на температурата</h1>
+          <p>Интелигентна система за управление на батерията, показва изправността на батерията и я защитава,
+            докато работи</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="4.jpg"></figure>
+          <h1>4,5-инчов LCD дисплей</h1>
+          <p>Персонализирайте вашите предпочитания за пътуване с интуитивен и информативен LCD дисплей. Направете
+            пътуването си сигурно чрез заключване на NFC карта с ключ</p>
+        </div>
+        <hr/>
+        <div class="grid-12">
+          <figure><img alt="MUKUTA" src="1.jpg"></figure>
+          <h1>Бързо сгъваем</h1>
+          <p>Нов силно обезопасен сгъваем носач с бързо заключване, кажете сбогом на клатещите се дръжки</p>
+        </div>
+        <hr/>
+        <hr/>
       </div>
     </div>
   </div>
-  <Footer/>
+  <!-- video -->
+  <div class="embed-container">
+    <iframe frameborder="0" src="https://youtube.com/embed/gU4TUxIjUFg"></iframe>
+  </div>
+
+
 </template>
 
 
@@ -38,6 +154,51 @@ export default {
 </script>
 <style scoped>
 
+
+.block-content {
+  background: black;
+}
+
+
+h1 {
+  color: #B6B3B6;
+  align-items: center;
+
+}
+
+img {
+  max-width: 100%;
+}
+
+p {
+  color: #B6B3B6;
+  font-size: 1.3em;
+}
+
+.text-center {
+  text-align: center;
+}
+
+
+.embed-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  padding-top: 0;
+  margin-top: 0;
+  overflow: hidden;
+  max-width: 100%;
+}
+
+.embed-container iframe, .embed-container object, .embed-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+
 .header-class {
   position: fixed;
   top: 0;
@@ -45,6 +206,7 @@ export default {
   width: 100%;
   z-index: 999;
 }
+
 .contact-page {
   max-width: 90vw;
   margin: 0 auto;
